fbe3d2b720fa7bd2006dbe1ba095f4b60068bbb3
gonsaiki <adityapotterz@gmail.com>
2020-02-07 19:13:20 +0700

    Berhasil menyelesaikan challange Equal Sum Checker with method

---
+++ Challange/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum (int one, int two, int three){
+        if (one + two == three){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
