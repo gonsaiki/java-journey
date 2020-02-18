481d2a2d62bee0c5b7a7a632dd97d673ef03c9fa
Aditya Fajri <adityapotterz@gmail.com>
2020-02-18 09:51:37 +0700

    delete main method

--- Easy Challenges/Prime Number Check/PrimeNumber.java
+++ Easy Challenges/Prime Number Check/PrimeNumber.java
@@ -1,24 +1,5 @@
 public class PrimeNumber {
-
-    public static void main(String[] args) {
-
-        int count = 0;
-        for (int i = 8; i < 100; i++) {
-            boolean timAsu = isPrime(i);
-
-            if (timAsu) {
-                count++;
-                System.out.println(i + " is prime number");
-            }
-
-            if (count == 3) {
-                break;
-            }
-        }
-
-        System.out.println("total prime number : " + count);
-    }
-
+    
     public static boolean isPrime(int a) {
         if (a == 1) {
             return false;
